b6e3d7c2cc6c08d5ad12aaec1b72fd5bd0a2ce0c
Ravi Netravali <ravinet@mit.edu>
2015-11-23 10:59:16 -0500

    simple standalone js file reads in JS source code line and prints AST

---
+++ rewrite.js
@@ -0,0 +1,12 @@
+// requires node packages listed below
+// example: 'npm install esprima'
+//
+esprima = require('esprima');
+estraverse = require('estraverse');
+util = require('util');
+
+fs = require('fs');
+
+var code = process.argv[2];
+var ast = esprima.parse(code, {loc: true});
+console.log(ast);
