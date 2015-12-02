8908ceaf2a224b10fdf5f33365509e5a3e1d3b9c
Ravi Netravali <ravinet@mit.edu>
2015-12-02 09:56:20 -0500

    handle individual source lines as well as entire files

--- process.js
+++ process.js
@@ -6,7 +6,14 @@ estraverse = require('estraverse');
 util = require('util');
 
 fs = require('fs');
-
-var code = process.argv[2];
+var type = process.argv[2];
+var code = '';
+if ( type == "line" ) {
+    code = process.argv[3];
+} else if ( type == "file" ) {
+    code = fs.readFileSync(process.argv[3]);
+} else {
+    throw "Must specify either 'line' or 'file'"
+}
 var ast = esprima.parse(code, {loc: true});
 console.log(JSON.stringify(ast, null, 4));
