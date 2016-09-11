036f62c16ae7c5cced1b6aabf62feda5ceffbfa6
Ravi Netravali <ravinet@mit.edu>
2016-09-11 18:37:00 -0400

    add emitter to simple running example

--- recording/nodejs_nondeterminism/justjs.js
+++ recording/nodejs_nondeterminism/justjs.js
@@ -1,4 +1,8 @@
+var events = require('events');
 var a = Math.random();
 var b = Date();
 console.log(a);
 console.log(b);
+const myEmitter = new events.EventEmitter();
+myEmitter.on('blah', function () {console.log('IN THE EVENT');});
+myEmitter.emit('blah');
