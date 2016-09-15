4c4f522b8ca82007a82e3296b0cb6b07954a4fe4
Ravi Netravali <ravinet@mit.edu>
2016-09-14 20:06:02 -0400

    modify node recording to log vector clock info as well

--- recording/nodejs_nondeterminism/record_shims.js
+++ recording/nodejs_nondeterminism/record_shims.js
@@ -6,6 +6,149 @@ var http = require('http');
 const util = require('util');
 delete Array.from;
 
+(function(){
+    var nextUniqueId = 0;
+
+    //numEntries: The number of clocks in the vector.
+    //localPos:   The local process' position in the vector.
+    //name:       [Optional] A unique name for this clock. The
+    //            name must actually be unique, so that we can
+    //            use names to break ties in VectorClock::generateTotalOrder().    
+    function VectorClock(numEntries, localPos, name){
+        this.clock = [];
+        this.name = name || nextUniqueId++;
+        this.localPos = localPos;
+        for(var i = 0; i < numEntries; i++){
+            this.clock.push(0);
+        }
+    }
+
+    //Call this method when a process is starting to
+    //handle an internal event. 
+    VectorClock.prototype.increment = function(){
+        this.clock[this.localPos]++;
+    };
+
+    //Returns whether this vector clock is causally ordered
+    //before another vector clock.
+    VectorClock.prototype.lessThan = function(otherClock){
+        if(this.clock.length != otherClock.clock.length){
+            throw "VectorClock::lessThan(): ERROR: The two clocks have different lengths.";
+        }
+        var atLeastOneSmaller = false;
+        for(var i = 0; i < this.clock.length; i++){
+            var mine = this.clock[i];
+            var theirs = otherClock.clock[i];
+            if(mine > theirs){
+                return false;
+            }
+            if(mine < theirs){
+                atLeastOneSmaller = true;
+            }
+        }
+        return atLeastOneSmaller;
+    };
+
+    //Returns whether two clocks happened at the same time
+    //with respect to logical time.
+    VectorClock.prototype.isConcurrent =  function(otherClock){
+        return (!this.lessThan(otherClock)) &&
+               (!otherClock.lessThan(this));
+    };
+
+    //Returns whether this clock happened before or concurrently
+    //with another clock.
+    VectorClock.prototype.lessThanOrEqual = function(otherClock){
+        return this.lessThan(otherClock) || this.isConcurrent(otherClock);
+    };
+
+    //Used to update this clock in response to the reception
+    //of another clock in a message.
+    VectorClock.prototype.update = function(clockFromMsg){
+        if(this.clock.length != clockFromMsg.clock.length){
+            throw "VectorClock::lessThan(): ERROR: The two clocks have different lengths.";
+        }
+        for(var i = 0; i < this.clock.length; i++){
+            if(i == this.localPos){
+                this.clock[i]++;
+            }else{
+                this.clock[i] = Math.max(this.clock[i], clockFromMsg.clock[i]);
+            }
+        }
+        // write the vector clock to the client cookie (so we don't have to do this everywhere!)
+        //this.writeToCookie();
+    };
+
+    //Returns a string with the individual clocks separated
+    //by colons, e.g., "4:23:0:1".
+    VectorClock.prototype.toString = function(){
+        var arr = [];
+        for(var i = 0; i < this.clock.length; i++){
+            arr.push(this.clock[i].toString());
+        }
+        return arr.join(":");
+    };
+
+    //Read the local cookies and extract the vector clock,
+    //looking for the clock inside the cookie key "vector-clock"
+    //unless a different key is explicitly provided. The
+    //localPos argument is required and indicates the position
+    //in the vector which the new VectorClock should associate
+    //with the local process.
+//    VectorClock.extractFromCookie = function(localPos, key){
+//        if(!localPos){
+//            throw "VectorClock::extractFromCookie(): ERROR: Must specify a localPos.";
+//        }
+//
+//        var cookies = document.cookie.split(";");
+//        key = key || "vector-clock";
+//        for(var i = 0; i < cookies.length; i++){
+//            var tokens = cookies[i].split("=");
+//            if(tokens[0] == key || tokens[0] == " vector-clock"){
+//                var val = tokens[1];
+//                tokens = val.split(":");
+//                var newClock = new VectorClock();
+//                for(var i = 0; i < tokens.length; i++){
+//                    newClock.clock[i] = parseInt(tokens[i]);
+//                }
+//                newClock.localPos = localPos;
+//                return newClock;
+//            }
+//        }
+//        return null;
+//    };
+
+    //Sorts the given log of {clock: VectorClockInstance, data: whatever}
+    //objects IN PLACE, i.e., the function does not return a new array,
+    //but instead sorts the array passed in as a parameter. Concurrent
+    //clocks are ordered using their .name property, so it's important
+    //for those properties to be unique!
+    VectorClock.generateTotalOrder = function(log){
+        var sortFunc = function(logEntry0, logEntry1){
+            var c0 = logEntry0.clock;
+            var c1 = logEntry1.clock;
+            if(c0.lessThan(c1)){
+                return -1;
+            }else if(c1.lessThan(c0)){
+                return 1;
+            }else{
+                if(c0.name < c1.name){
+                    return -1;
+                }else if(c0.name > c1.name){
+                    return 1;
+                }else{
+                    throw "VectorClock::generateTotalOrder: ERROR: Could not break tie . . . are clock names non-unique?";
+                }
+            }
+        };
+        log.sort(sortFunc);
+    };
+
+    global.VectorClock = VectorClock;
+})();
+    // create vector clock for client (has entries for client and server)
+    var server_clock = new VectorClock(2,0);
+
 global.http_events = ['response', 'end', 'finish', '_socketEnd', 'connect', 'free', 'close', 'agentRemove', 'socket', 'drain', 'data', 'prefinish', 'SIGWINCH'];
 global.http_emits = ['socket', 'prefinish', 'resume', 'lookup', 'finish', 'connect', 'data', 'readable', 'end', 'close'];
 
@@ -33,10 +176,10 @@ global.setTimeout = function (func, delay) {
     var stack = new Error().stack.split("\n")[1].split(":");
     var line = stack[stack.length - 2];
     var curr_id = unique_timeout_ids;
-    var wrapper_func = function() {var hrTime = process.hrtime();var log_timeout = {'Function': 'setTimeout', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000};shim_logs.push(JSON.stringify(log_timeout));func();};
+    var wrapper_func = function() {server_clock.increment();var hrTime = process.hrtime();var log_timeout = {'Function': 'setTimeout', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_timeout));func();};
     if ( typeof(func) == "string" ) {
         var make_func = new Function(func);
-        var wrapper_func = function() {var log_timeout = {'Function': 'setTimeout', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000};shim_logs.push(JSON.stringify(log_timeout));make_func();};
+        var wrapper_func = function() {server_clock.increment();var log_timeout = {'Function': 'setTimeout', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_timeout));make_func();};
     }
     var retVal = _settimeout(wrapper_func, delay);
     unique_timeout_id_mappings[curr_id] = retVal;
@@ -50,10 +193,10 @@ global.setInterval = function (func, delay) {
     var stack = new Error().stack.split("\n")[1].split(":");
     var line = stack[stack.length - 2];
     var curr_id = unique_timeout_ids;
-    var wrapper_func = function() {var hrTime = process.hrtime();var log_timeout = {'Function': 'setInterval', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000};shim_logs.push(JSON.stringify(log_timeout));func();};
+    var wrapper_func = function() {server_clock.increment();var hrTime = process.hrtime();var log_timeout = {'Function': 'setInterval', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_timeout));func();};
     if ( typeof(func) == "string" ) {
         var make_func = new Function(func);
-        var wrapper_func = function() {var log_timeout = {'Function': 'setInterval', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000};shim_logs.push(JSON.stringify(log_timeout));make_func();};
+        var wrapper_func = function() {server_clock.increment();var log_timeout = {'Function': 'setInterval', 'OrigLine': line, 'UniqueID': curr_id, 'TimeoutId': unique_timeout_id_mappings[curr_id], 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_timeout));make_func();};
     }
     var retVal = _setinterval(wrapper_func, delay);
     unique_timeout_id_mappings[curr_id] = retVal;
@@ -71,7 +214,7 @@ events.EventEmitter.prototype.on = function (type, listener) {
     if ( global.http_events.indexOf(type) != -1 ) { // http event we care about, handle appropriately! log info to fire again in replay
     }
     var args = from(arguments);
-    var wrapper_func = function() {var keep_req = {};if ( this instanceof http.ClientRequest ) {for (var d in global.request_info ) {keep_req[global.request_info[d]] = this[global.request_info[d]];}}var keep_res = {};if ( this instanceof http.IncomingMessage ) {for (var d in global.response_info ) {keep_res[global.response_info[d]] = this[global.response_info[d]];}}var hrTime = process.hrtime();var log_event = {'Function': 'EventEmitter.on', 'OrigLine': line, 'EventType': type, 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'ReqInfo': JSON.stringify(keep_req), 'ResInfo': JSON.stringify(keep_res)};shim_logs.push(JSON.stringify(log_event));var args = from(arguments);listener.apply(this, args);};
+    var wrapper_func = function() {server_clock.increment();var keep_req = {};if ( this instanceof http.ClientRequest ) {for (var d in global.request_info ) {keep_req[global.request_info[d]] = this[global.request_info[d]];}}var keep_res = {};if ( this instanceof http.IncomingMessage ) {for (var d in global.response_info ) {keep_res[global.response_info[d]] = this[global.response_info[d]];}}var hrTime = process.hrtime();var log_event = {'Function': 'EventEmitter.on', 'OrigLine': line, 'EventType': type, 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'ReqInfo': JSON.stringify(keep_req), 'ResInfo': JSON.stringify(keep_res), 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_event));var args = from(arguments);listener.apply(this, args);};
     args1 = [type, wrapper_func];
     var retVal = _eventemitteron.apply(this, args1);
     return retVal;
@@ -84,7 +227,7 @@ events.EventEmitter.prototype.once = function (type, listener) {
     var line = stack[stack.length - 2];
     var curr_id = global.handler_ids;
     global.handler_ids += 1;
-    var wrapper_func = function() {var keep_req = {};if ( this instanceof http.ClientRequest ) {for (var d in global.request_info ) {keep_req[global.request_info[d]] = this[global.request_info[d]];}}var keep_res = {};if ( this instanceof http.IncomingMessage ) {for (var d in global.response_info ) {keep_res[global.response_info[d]] = this[global.response_info[d]];}}var hrTime = process.hrtime();var log_event = {'Function': 'EventEmitter.once', 'OrigLine': line, 'EventType': type, 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'ReqInfo': JSON.stringify(keep_req), 'ResInfo': JSON.stringify(keep_res)};shim_logs.push(JSON.stringify(log_event));var args = from(arguments);listener.apply(this, args);};
+    var wrapper_func = function() {server_clock.increment();var keep_req = {};if ( this instanceof http.ClientRequest ) {for (var d in global.request_info ) {keep_req[global.request_info[d]] = this[global.request_info[d]];}}var keep_res = {};if ( this instanceof http.IncomingMessage ) {for (var d in global.response_info ) {keep_res[global.response_info[d]] = this[global.response_info[d]];}}var hrTime = process.hrtime();var log_event = {'Function': 'EventEmitter.once', 'OrigLine': line, 'EventType': type, 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'ReqInfo': JSON.stringify(keep_req), 'ResInfo': JSON.stringify(keep_res), 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_event));var args = from(arguments);listener.apply(this, args);};
     var args = [type, wrapper_func];
     var retVal = _eventemitteronce.apply(this, args);
     return retVal;
@@ -97,7 +240,7 @@ events.EventEmitter.prototype.prependListener = function (type, listener) {
     var line = stack[stack.length - 2];
     var curr_id = global.handler_ids;
     global.handler_ids += 1;
-    var wrapper_func = function() {var keep_req = {};if ( this instanceof http.ClientRequest ) {for (var d in global.request_info ) {keep_req[global.request_info[d]] = this[global.request_info[d]];}}var keep_res = {};if ( this instanceof http.IncomingMessage ) {for (var d in global.response_info ) {keep_res[global.response_info[d]] = this[global.response_info[d]];}}var hrTime = process.hrtime();var log_event = {'Function': 'EventEmitter.prependListener', 'OrigLine': line, 'EventType': type, 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'ReqInfo': JSON.stringify(keep_req), 'ResInfo': JSON.stringify(keep_res)};shim_logs.push(JSON.stringify(log_event));var args = from(arguments);listener.apply(this, args);};
+    var wrapper_func = function() {server_clock.increment();var keep_req = {};if ( this instanceof http.ClientRequest ) {for (var d in global.request_info ) {keep_req[global.request_info[d]] = this[global.request_info[d]];}}var keep_res = {};if ( this instanceof http.IncomingMessage ) {for (var d in global.response_info ) {keep_res[global.response_info[d]] = this[global.response_info[d]];}}var hrTime = process.hrtime();var log_event = {'Function': 'EventEmitter.prependListener', 'OrigLine': line, 'EventType': type, 'Time': hrTime[0] * 1000000 + hrTime[1] / 1000, 'ReqInfo': JSON.stringify(keep_req), 'ResInfo': JSON.stringify(keep_res), 'Vector_Clock': server_clock.toString()};shim_logs.push(JSON.stringify(log_event));var args = from(arguments);listener.apply(this, args);};
     var args = [type, wrapper_func];
     var retVal = _eventemitterprepend.apply(this, args);
     return retVal;
